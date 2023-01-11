479255781f1ea3c53bd3322655b77f3922407068
Kero <73374143+Krogmh4u@users.noreply.github.com>
2023-01-11 17:56:51 +0100

    fixed main

--- app/main.cpp
+++ app/main.cpp
@@ -9,28 +9,6 @@ int __stdcall wWinMain(
 	int commandShow
 )
 {
-	W_SPACE space; 
-	strcpy(space.Name, "SMPTE-C RGB");
-	strcpy(space.RefWhite, "D65");
-	for (auto i = 0; i < 9; ++i)
-		space.RGB2XYZ_Matrix[i] = TrMatrixes::smpte_cRgb[i];
-	space.Gamma = 2.2f;
-	space.LabGamutEfficiency = 31.9f;
-	space.CodingEfficiency = 100.0f;
-	space.Illuminant[0] = 0.95047f;
-	space.Illuminant[1] = 1.00000f;
-	space.Illuminant[2] = 1.08883f;
-
-	std::fstream fout("smtpec.ws", std::ios::out | std::ios::binary);
-
-	if (fout.is_open())
-	{
-		fout.seekg(fout._Seekbeg);
-		fout.write((char*)&space, sizeof(W_SPACE));
-
-		fout.close();
-	}
-
 	gui::CreateHWindow("GAMUTO", "GAMUTOCLASS");
 	gui::CreateDevice();
 	gui::CreateImGui();
